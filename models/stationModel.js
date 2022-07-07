258df1e2320e11f9c2a161e7af05aa6ed8d0d323
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-07-07 14:25:51 -0400

    modified station model removed unique from image and name

--- models/stationModel.js
+++ models/stationModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const StationSchema = new mongoose.Schema({
-  image: { type: String, required: true, unique: true,},
-  name: { type: String, required: true, unique: true },
+  image: { type: String, required: true },
+  name: { type: String, required: true },
   address: { type: String, required: true, unique: true },
   lat: { type: String, required: false },
   long: { type: String, required: false },
